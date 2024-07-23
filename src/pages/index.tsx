0f724c529cc4b83849918bfe6f1cb8175b0b5f3c
ankit-Kumar-4 <ankitkumar4898@gmail.com>
2024-07-23 11:39:19 +0530

    updated home page image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageComponent = () => {
     return (
-        <img src="https://i.imgflip.com/8wwag7.jpg" alt="Next in Queue" className='max-w-full h-auto border-2 border-gray-300 rounded-md shadow-md' />
+        <img src="https://i.imgflip.com/8xwsi9.jpg" alt="Next in Queue" className='max-w-full h-auto border-2 border-gray-300 rounded-md shadow-md' />
     );
 }
 
