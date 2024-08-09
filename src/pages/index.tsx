018feb1ce583ee604f87abb640e8d0f5b252d728
ankit-Kumar-4 <ankitkumar4898@gmail.com>
2024-08-10 00:17:27 +0530

    updated home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 const ImageComponent = () => {
     return (
-        <img src="https://i.imgflip.com/8xwsi9.jpg" alt="Next in Queue" className='max-w-full h-auto border-2 border-gray-300 rounded-md shadow-md' />
+        <div className="relative inline-block">
+            <img
+                src="https://i.imgflip.com/2wgh5p.png"
+                alt="Responsive image"
+                className="block w-full h-auto"
+            />
+            <div className="absolute top-8 left-7 md:top-24 md:left-28 text-blue-100 md:text-3xl">
+                <Link href="/tic-tac-toe" legacyBehavior>Tic-tac-toe</Link>
+            </div>
+            <div className="absolute top-14 right-16 md:top-36 md:right-52 text-gray-300 md:text-3xl">
+                <Link href="/wordle" legacyBehavior>Wordle</Link>
+
+            </div>
+            <div className="absolute top-32 left-24 text-wrap text-gray-300 md:text-3xl">
+                <Link href="/snake-n-ladder" legacyBehavior>Snake-n-Ladder</Link>
+
+            </div>
+            <div className="absolute top-44 right-8 text-gray-300 md:text-3xl">
+                <Link href="/2048" legacyBehavior>2048</Link>
+
+            </div>
+            <div className="absolute top-52 left-14 text-gray-300 md:text-3xl">
+                <Link href="/sudoku" legacyBehavior>Sudoku</Link>
+
+            </div>
+            <div className="absolute top-64 right-28 text-black md:text-3xl">
+                <Link href="https://asutosh-tetris.vercel.app/" legacyBehavior>
+                    <a target="_blank" rel="noopener noreferrer">Tetris</a>
+                </Link>
+            </div>
+            <div className='absolute bottom-10 left-14 text-white'> My Website</div>
+        </div>
     );
 }
 
