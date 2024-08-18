ccaed26d24bc5d4c4a5b5f6703be28974a121c3f
ankit-Kumar-4 <ankitkumar4898@gmail.com>
2024-08-18 17:26:23 +0530

    added private chat

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,22 +1,31 @@
 // components/ChatWindow.tsx
+import { v4 as uuidv4 } from 'uuid';
 import { useEffect, useState } from 'react';
 import { auth, db } from '@/lib/firebaseConfig';
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import {
     collection,
     addDoc,
+    doc,
+    updateDoc,
+    arrayUnion,
+    getDoc,
     query,
     orderBy,
     limit,
     onSnapshot,
 } from 'firebase/firestore';
+import { createChatroom, joinChatroom, sendPrivateMessage, usePrivateMessage } from '@/lib/chatroom';
 import ProtectedRoute from '@/components/ProtectedRoute';
 
 
 export default function ChatWindow({ onClose }: { onClose: () => void }) {
     const [user, setUser] = useState<any>(null);
-    const [messages, setMessages] = useState<any[]>([]);
-    const [newMessage, setNewMessage] = useState('');
+    const [chat, setChat] = useState<any[]>([]);
+    const [privateChat, setPrivateChat] = useState<any[]>([]);
+    const [input, setInput] = useState('');
+    const [activeTab, setActiveTab] = useState('global');
+    const [isChatActive, setIsChatActive] = useState(false);
+    const [chatroomId, setChatroomID] = useState('');
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -36,7 +45,7 @@ export default function ChatWindow({ onClose }: { onClose: () => void }) {
             querySnapshot.forEach((doc) => {
                 messagesData.push({ id: doc.id, ...doc.data() });
             });
-            setMessages(messagesData.reverse());
+            setChat(messagesData.reverse());
         });
 
         return () => {
@@ -45,24 +54,64 @@ export default function ChatWindow({ onClose }: { onClose: () => void }) {
         };
     }, []);
 
+    useEffect(() => {
+        if (isChatActive) {
+            const unsubscribe = onSnapshot(
+                query(
+                    collection(db, "chatrooms", chatroomId, "messages"),
+                    orderBy("timeStamp", "asc")
+                ),
+                (snapshot) => {
+                    const newMessages = snapshot.docs.map(doc => doc.data());
+                    setPrivateChat(newMessages);
+                }
+            );
+
+            return () => unsubscribe();
+        }
+    }, [chatroomId, isChatActive]);
+
+
     const handleSendMessage = async () => {
-        if (newMessage.trim() === '') return;
+        if (input.trim() === '') return;
 
-        await addDoc(collection(db, 'messages'), {
-            text: newMessage,
-            uid: user.uid,
-            email: user.email,
-            displayName: user.displayName,
-            timestamp: new Date(),
-        });
+        if (isChatActive) {
+            await sendPrivateMessage(chatroomId, input);
+        } else {
+            await addDoc(collection(db, 'messages'), {
+                text: input,
+                uid: user.uid,
+                email: user.email,
+                displayName: user.displayName,
+                timestamp: new Date(),
+            });
+        }
 
-        setNewMessage('');
+        setInput('');
     };
+
+    const handleChatroomCreation = async () => {
+        const chatroom_id = await createChatroom();
+        setIsChatActive(true);
+        setChatroomID(chatroom_id);
+        alert('Please share chatromm id with friends: ' + chatroom_id);
+        sendPrivateMessage(chatroom_id, 'Please share chatroom id: ' + chatroom_id)
+    }
+
+    const handleChatroomJoining = async () => {
+        const result = await joinChatroom(chatroomId);
+        if (result) {
+            alert('Welcome to the chat!')
+        } else {
+            alert('Invalid chatroom ID')
+        }
+        setIsChatActive(true);
+    }
 
     return (
         <ProtectedRoute>
             {user ? (
-                <div className="fixed bottom-16 right-4 bg-white w-80 h-96 shadow-lg rounded-lg p-4 flex flex-col">
+                <div className="fixed bottom-16 right-4 bg-white w-80 h-4/6 shadow-lg rounded-lg p-4 flex flex-col">
                     <div className="flex justify-between items-center border-b pb-2 mb-4">
                         <h3 className="text-lg font-semibold">Welcome, {user.displayName || user.email || user.uid}</h3>
                         <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
@@ -70,9 +119,28 @@ export default function ChatWindow({ onClose }: { onClose: () => void }) {
                         </button>
                     </div>
 
-                    
+                    {/* Tab Navigation */}
+                    <div className="flex justify-around mb-4">
+                        <button
+                            className={`w-full py-2 ${activeTab === "global" ? "border-b-2 border-blue-500" : ""
+                                }`}
+                            onClick={() => setActiveTab("global")}
+                        >
+                            Global Chat
+                        </button>
+                        <button
+                            className={`w-full py-2 ${activeTab === "private" ? "border-b-2 border-blue-500" : ""
+                                }`}
+                            onClick={() => setActiveTab("private")}
+                        >
+                            Private Chat
+                        </button>
+                    </div>
+
+
+                    {/* chat content */}
                     <div className="flex-1 overflow-y-auto mb-4">
-                        {messages.map((message) => (
+                        {activeTab === 'global' ? chat.map((message) => (
                             <div
                                 key={message.id}
                                 className={`${message.uid === user.uid
@@ -83,13 +151,59 @@ export default function ChatWindow({ onClose }: { onClose: () => void }) {
                                 <span className="font-bold">{message.displayName || message.email || message.uid}:</span>{' '}
                                 {message.text}
                             </div>
-                        ))}
+                        )) : isChatActive ? (
+                            privateChat.map((message) => (
+                                <div
+                                    key={message.id}
+                                    className={`${message.uid === user.uid
+                                        ? 'text-right'
+                                        : 'text-left'
+                                        } mb-2`}
+                                >
+                                    <span className="font-bold">{message.displayName || message.email || message.uid}:</span>{' '}
+                                    {message.text}
+                                </div>
+                            ))
+                        ) : (
+                            <div className="flex-col justify-around mb-4">
+                                <button
+                                    className={`w-full py-2 ${activeTab === "global" ? "border-b-2 border-blue-500" : ""
+                                        }`}
+                                    onClick={() => handleChatroomCreation()}
+                                >
+                                    Create Chatroom
+                                </button>
+                                <div className='text-center m-5'>OR</div>
+                                <input
+                                    type="text"
+                                    value={chatroomId}
+                                    onChange={(e) => {
+                                        const newValue = e.target.value.toUpperCase();
+                                        if (/^[A-Z0-9]*$/.test(newValue) && newValue.length <= 5) {
+                                            setChatroomID(newValue);
+                                        }
+                                    }}
+                                    className="flex-grow p-2 w-full border rounded"
+                                    pattern="[A-Z0-9]{5}"
+                                />
+                                <button
+                                    className={`w-full py-2 ${activeTab === "private" ? "border-b-2 border-blue-500" : ""
+                                        }`}
+                                    onClick={() => handleChatroomJoining()}
+                                >
+                                    Join Chat with ID
+                                </button>
+                            </div>
+                        )}
                     </div>
+
+
+                    {/* Input field */}
                     <div className="flex mt-4">
                         <input
                             type="text"
-                            value={newMessage}
-                            onChange={(e) => setNewMessage(e.target.value)}
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
                             className="flex-grow p-2 border rounded"
                         />
                         <button
--- src/lib/chatroom.ts
+++ src/lib/chatroom.ts
@@ -1,12 +1,30 @@
 import { v4 as uuidv4 } from 'uuid';
-import { collection, addDoc, doc, updateDoc, arrayUnion, getDoc, onSnapshot } from "firebase/firestore";
+import {
+    collection,
+    addDoc,
+    doc,
+    updateDoc,
+    arrayUnion,
+    getDoc,
+    onSnapshot,
+    setDoc
+} from "firebase/firestore";
 import { db, auth } from '@/lib/firebaseConfig'; // Firestore instance
 import { useEffect, useState } from 'react';
 
+function generateRandomString(): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let result = '';
+    for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+}
+
 export async function createChatroom() {
-    const chatroomId = uuidv4();
-    await addDoc(collection(db, "chatrooms"), {
-        id: chatroomId,
+    const chatroomId = generateRandomString();
+    await setDoc(doc(db, "chatrooms", chatroomId), {
         createdBy: auth.currentUser?.uid,
         participants: [auth.currentUser?.uid],
         createdAt: new Date(),
@@ -23,21 +41,24 @@ export async function joinChatroom(chatroomId: string) {
         await updateDoc(chatroomRef, {
             participants: arrayUnion(auth.currentUser?.uid),
         });
+        return true;
     } else {
-        throw new Error("Chatroom not found");
+        return false;
     }
 }
 
 
-export async function sendMessage(chatroomId: string, message: string) {
+export async function sendPrivateMessage(chatroomId: string, message: string) {
     await addDoc(collection(db, "chatrooms", chatroomId, "messages"), {
         text: message,
-        sender: auth.currentUser?.uid,
-        createdAt: new Date(),
+        uid: auth.currentUser?.uid,
+        email: auth.currentUser?.email,
+        displayName: auth.currentUser?.displayName,
+        timeStamp: new Date(),
     });
 }
 
-export function useMessages(chatroomId: string) {
+export function usePrivateMessage(chatroomId: string) {
     const [messages, setMessages] = useState<any[]>([]);
 
     useEffect(() => {
