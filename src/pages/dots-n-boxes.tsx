dd7318b40cbec6c461c34bb272f26c5adf20450e
ankit-Kumar-4 <ankitkumar4898@gmail.com>
2024-09-10 23:05:45 +0530

    dynamic board size in dots-n-boxes

--- src/pages/dots-n-boxes.tsx
+++ src/pages/dots-n-boxes.tsx
@@ -31,8 +31,53 @@ const Board = ({ row, column }: { row: number; column: number }) => {
         }
     }
 
+    let rowSize = 'grid-rows-3';
+    switch (row) {
+        case 3:
+            rowSize = 'grid-rows-3'
+            break;
+        case 5:
+            rowSize = 'grid-rows-5'
+            break;
+        case 7:
+            rowSize = 'grid-rows-7'
+            break;
+        case 9:
+            rowSize = 'grid-rows-9'
+            break;
+        case 11:
+            rowSize = 'grid-rows-11'
+            break;
+        default:
+            rowSize = 'grid-rows-5'
+            break;
+    }
+
+    let colSize = 'grid-cols-3';
+    switch (column) {
+        case 3:
+            colSize = 'grid-cols-3'
+            break;
+        case 5:
+            colSize = 'grid-cols-5'
+            break;
+        case 7:
+            colSize = 'grid-cols-7'
+            break;
+        case 9:
+            colSize = 'grid-cols-9'
+            break;
+        case 11:
+            colSize = 'grid-cols-11'
+            break;
+        default:
+            colSize = 'grid-cols-5'
+            break;
+    }
+
     return (
-        <div className={`grid grid-cols-${column} grid-rows-${row} gap-0`}>
+        <div className={`grid gap-0 max-h-full`}
+            style={{ gridTemplateColumns: `repeat(${column}, minmax(0, 1fr))`, gridTemplateRows: `repeat(${row}, minmax(0, 1fr))` }}>
             {board}
 
         </div>
@@ -43,15 +88,20 @@ export default function Game() {
     const [row, setRow] = useState(5);
     const [column, setColumn] = useState(5);
 
-    const options = [3, 5, 7, 9];
+    const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
     return (
-        <div className="flex flex-col items-center justify-center max-h-screen ">
-            <Dropdown options={options} value={row} setValue={setRow}/>
-            <Dropdown options={options} value={column} setValue={setColumn}/>
-            <div className="m-2"></div>
-            <Board row={row} column={column} />
-        </div>
+        <>
+            <div className="pl-36">
+
+                <Dropdown options={options} value={row} setValue={setRow} />
+                <Dropdown options={options} value={column} setValue={setColumn} />
+            </div>
+            <div className="flex flex-col items-center justify-center max-h-screen ">
+                <div className="m-2"></div>
+                <Board row={2 * row + 1} column={2 * column + 1} />
+            </div>
+        </>
 
 
     )
