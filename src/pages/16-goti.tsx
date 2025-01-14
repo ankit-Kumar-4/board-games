4a1e3120536fa27ba552c9ce50cf3f8571c71165
ankit-Kumar-4 <ankitkumar4898@gmail.com>
2025-01-14 16:18:01 +0530

    dynamic board for 16 goti complete

--- src/pages/16-goti.tsx
+++ src/pages/16-goti.tsx
@@ -10,32 +10,40 @@ const unused_dots = [1, 7, 9, 17, 27, 35, 37, 43];
 const Dot = ({ index }: { index: number }) => {
     return (
         <div id={`${index}`}
-            className={`w-2 h-2 md:w-8 md:h-8 rounded-full z-20
+            className={`w-3 h-3 md:w-8 md:h-8 rounded-full z-20
                 ${unused_dots.includes(index) ? '' : 'bg-black'}`}
         ></div>
     )
 }
 
-const Dash = ({ index, value, highlight }: {
-    index: number, value: number, highlight: boolean
+const Dash = ({ index, value, highlight, isPortrait }: {
+    index: number, value: number, highlight: boolean, isPortrait: boolean
 }) => {
+    let temp = unused_dashes;
+    if (isPortrait) {
+        temp = unused_strokes
+    }
     return (
         <div
-            className={`h-2 w-8 md:h-8 md:w-36 ${highlight ? 'border-2 border-black' : ''} 
-                ${unused_dashes.includes(index) ? '' :
+            className={`h-3 w-12 md:h-8 md:w-32 ${highlight ? 'border-2 border-black' : ''} 
+                ${temp.includes(index) ? '' :
                     (value === null ? 'bg-white' : (value === 0 ? 'bg-blue-500' : 'bg-red-600'))}
             `}
         ></div>
     )
 }
 
-const Stroke = ({ index, value, highlight }: {
-    index: number, value: number, highlight: boolean
+const Stroke = ({ index, value, highlight, isPortrait }: {
+    index: number, value: number, highlight: boolean, isPortrait: boolean
 }) => {
+    let temp = unused_strokes;
+    if (isPortrait) {
+        temp = unused_dashes;
+    }
     return (
         <div
-            className={`w-2 h-8 md:w-8 md:h-36 ${highlight ? 'border-2 border-black' : ''} 
-                ${unused_strokes.includes(index) ? '' : (value === null ? 'bg-white' : (value === 0 ? 'bg-blue-500' : 'bg-red-600'))}
+            className={`w-3 h-8 md:w-8 md:h-32 ${highlight ? 'border-2 border-black' : ''} 
+                ${temp.includes(index) ? '' : (value === null ? 'bg-white' : (value === 0 ? 'bg-blue-500' : 'bg-red-600'))}
             `}
         ></div>
     )
@@ -86,12 +94,24 @@ const Board = ({ row, column, dashes, strokes, isPortrait }:
                     board.push(<Dot key={key} index={temp} />);
                     dt++;
                 } else {
-                    board.push(<Dash key={key} index={d} highlight={false} value={dashes[d]} />);
+                    let temp = d;
+                    if (isPortrait) {
+                        let idx = Math.floor(d / 4);
+                        let col = d % 4;
+                        temp = getConvertedIndex(idx, col, 9);
+                    }
+                    board.push(<Dash key={key} index={temp} highlight={false} value={dashes[d]} isPortrait={isPortrait} />);
                     d++;
                 }
             } else {
                 if (j % 2 == 0) {
-                    board.push(<Stroke key={key} index={s} highlight={false} value={strokes[s]} />);
+                    let temp = s;
+                    if (isPortrait) {
+                        let idx = Math.floor(s / 5);
+                        let col = s % 5;
+                        temp = getConvertedIndex(idx, col, 8);
+                    }
+                    board.push(<Stroke key={key} index={temp} highlight={false} value={strokes[s]} isPortrait={isPortrait} />);
                     s++;
                 } else {
                     board.push(<Box key={key} />);
